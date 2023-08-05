b8e5e365d0b6d00cc1a5ac1d30a6828b328aa801
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-08-05 11:50:17 +1000

    Get and post operations

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hackathons-api
 
-## ERD
+## 03/08 lesson. SQL 
+
+### ERD
 
 In this Hackathon environment ERD, projects are created and can have multiple members assigned to them. Each member can only be associated with one project. Projects can also have a tech stack consisting of multiple technologies, and each technology can be used in multiple projects. This ERD helps students understand the relationships between projects, members, and technologies within a hackathon context.
 
@@ -12,3 +14,20 @@ In this Hackathon environment ERD, projects are created and can have multiple me
 - Simple queries.
 - Delete and update.
 - Create a script.
+
+## 05/08 lesson. ORM
+
+We will create a new database today. The purpose is still to store information about the Hackathon, but also start creating the API with Flask. Using an ORM (SQLAlchemy) we will be able to connect the app to a database and do all the db related tasks we already know from the app. In order to learn how to create the tables from scratch and to avoid conflicts with the previous database, we will create a new one.
+
+- Create the project (venv, flask, and first run)
+- Connect to a database (create, user, set the connection with flask)
+- Create a project model and a table
+- Seed tables command
+- Drop tables command
+- Schemas
+- Get all projects
+- ...
+- Find a project by id
+- Create a project
+- Delete a project
+- Update a project 
---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, request #, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+app = Flask(__name__)
+ma = Marshmallow(app)
+
+# what dbms + db adapter + db_user + password + host:port + database name
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://hackathon_db_admin:password123@localhost:5432/hackathon_db_flask"
+
+#create the database instance
+db = SQLAlchemy(app)
+
+@app.cli.command("create")
+def create_tables():
+    db.create_all()
+    print("tables created")
+
+@app.cli.command("seed")
+def seed_tables():
+    project1 = Project(
+        title = 'Brisbane Traffic Solver',
+        repository = 'https://github.com/traffic_team/traffic_solver', 
+        description = 'description goes here...'
+    )
+    db.session.add(project1)
+
+    project2 = Project(
+        title = 'Sustainability coding board game',
+        repository = 'https://github.com/ca_team/coding_board_game', 
+        description = 'description goes here...'
+    )
+    db.session.add(project2)
+    db.session.commit()
+    print("tables seeded")
+
+@app.cli.command('drop')
+def drop_tables():
+    db.drop_all()
+    print("tables dropped")
+
+class Project(db.Model):
+    __tablename__ = "projects"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String())
+    repository = db.Column(db.String())
+    description = db.Column(db.String())
+
+class ProjectSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "repository", "description")
+
+projects_schema = ProjectSchema(many=True)
+project_schema = ProjectSchema()
+
+
+@app.route('/')
+def welcome():
+    return "Welcome students to the hackathon!"
+
+#@app.route('/projects', methods=["GET"])
+#@app.route('/projects')
+@app.get('/projects')
+def get_projects():
+    #prepare the query to get data SELECT * FROM PROJECTS
+    stmt = db.select(Project)
+    #get the data
+    projects = db.session.scalars(stmt)
+    #convert the db data into something readable by Python
+    result = projects_schema.dump(projects)
+
+    return result
+    #return jsonify(result)
+
+@app.get('/projects/<int:id>/')
+def get_project_by_id(id):
+    #prepare the query to get data SELECT * FROM PROJECTS WHERE ID = :id
+    stmt = db.select(Project).filter_by(id = id)
+    #get the data
+    project = db.session.scalar(stmt)
+
+    if project:
+        #convert the db data into something readable by Python
+        return project_schema.dump(project)
+    else:
+        return {'error' : f'Project not found with id {id}'}, 404
+
+#@app.route('/projects', methods=["POST"])
+@app.post('/projects')
+def create_project():
+   #create a project
+   #print(request.json)
+   project_fields = project_schema.load(request.json)
+   new_project = Project(
+        title = project_fields["title"],
+        repository = project_fields["repository"], 
+        description = project_fields["description"]
+    )
+   db.session.add(new_project)
+   db.session.commit() 
+   return project_schema.dump(new_project), 201
